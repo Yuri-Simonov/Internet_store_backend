897000b5928a9bbf93fed74fed8b03b6daad1656
Yuri Simonov <y.simonov@alef.im>
2023-03-12 15:52:07 +0300

    первичная разработка завершена

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 
 import {
 	registerValidation,
@@ -7,10 +8,9 @@ import {
 	postCreateValidation,
 } from "./validations.js";
 
-import checkAuth from "./utils/check_auth.js";
+import { checkAuth, handleValidationErrors } from "./utils/index.js";
 
-import * as UserController from "./controllers/UserController.js";
-import * as PostController from "./controllers/PostController.js";
+import { UserController, PostController } from "./controllers/index.js";
 
 mongoose
 	.connect(
@@ -21,21 +21,61 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+	destination: (_, __, cb) => {
+		cb(null, "uploads");
+	},
+	filename: (_, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
 	res.send("Hello fucking world!");
 });
 
-app.post("/auth/login", loginValidation, UserController.login);
-app.post("/auth/register", registerValidation, UserController.register);
+app.post(
+	"/auth/login",
+	loginValidation,
+	handleValidationErrors,
+	UserController.login
+);
+app.post(
+	"/auth/register",
+	registerValidation,
+	handleValidationErrors,
+	UserController.register
+);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+	res.json({
+		url: `/uploads/${req.file.originalname}`,
+	});
+});
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
-app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.post(
+	"/posts",
+	checkAuth,
+	postCreateValidation,
+	handleValidationErrors,
+	PostController.create
+);
 app.delete("/posts/:id", checkAuth, PostController.remove);
-app.patch("/posts/:id", checkAuth, PostController.update);
+app.patch(
+	"/posts/:id",
+	checkAuth,
+	postCreateValidation,
+	handleValidationErrors,
+	PostController.update
+);
 
 app.listen(4444, (err) => {
 	if (err) return console.log(err);
---
+++ utils/index.js
@@ -0,0 +1,2 @@
+export { default as checkAuth } from "./check_auth.js";
+export { default as handleValidationErrors } from "./handleValidationErrors.js";
